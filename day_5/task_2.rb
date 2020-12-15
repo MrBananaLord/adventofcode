de72c528b856d202fa5e21560489155e88db6a6f
jan-bator <jan.bator@xing.com>
2020-12-15 08:40:48 +0100

    Parsing refactor.

--- day_5/task_2.rb
+++ day_5/task_2.rb
@@ -1,19 +1,16 @@
 cards = File.read("input.txt").split("\n")
 
-def parse_code(string)
-  string.split("").inject(0..(2 ** string.length - 1)) do |position, letter|
-    if %w(F L).include?(letter)
-      position = position.first..(position.last - (position.count / 2))
-    elsif %w(B R).include?(letter)
-      position = (position.first + (position.count / 2))..position.last
-    end
+def code_to_number(string)
+  string.split("").each_with_index.inject(0) do |sum, (letter, index)|
+    sum += 2 ** (string.length - index - 1) if %w(B R).include?(letter)
 
-  end.first
+    sum
+  end
 end
 
 result = cards.map do |card|
-  row = parse_code(card.slice(0,7))
-  column = parse_code(card.slice(7,3))
+  row = code_to_number(card.slice(0,7))
+  column = code_to_number(card.slice(7,3))
 
   {
     row: row,
@@ -33,4 +30,4 @@ seat = result.group_by { |e| e[:row] }.filter_map do |row, seats|
   end
 end.first
 
-pp seats
+pp seat
