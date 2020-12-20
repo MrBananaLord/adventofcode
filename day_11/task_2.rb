80279faf5ced739779a0bd3354984c953d682cec
jan-bator <jan.bator@xing.com>
2020-12-20 10:26:44 +0100

    Benchmark the whole run.

--- day_11/task_2.rb
+++ day_11/task_2.rb
@@ -1,5 +1,4 @@
 DIRECTIONS = [[0, 1], [0, -1], [1, 0], [1, 1], [1, -1], [-1, 0], [-1, 1], [-1, -1]]
-places = File.read("input.txt").split("\n").map { |line| line.split("") }
 
 def visible_seats(places, starting_row_index, starting_column_index)
   DIRECTIONS.map do |row_offset, column_offset|
@@ -18,12 +17,6 @@ def visible_seats(places, starting_row_index, starting_column_index)
       distance += 1
     end
   end.compact
-end
-
-$visible_seats = places.each_with_index.map do |row, row_index|
-  row.each_with_index.map do |place, column_index|
-    visible_seats(places, row_index, column_index)
-  end
 end
 
 def visible_place_states(places, row_index, column_index)
@@ -53,11 +46,20 @@ end
 require 'benchmark'
 
 time = Benchmark.measure {
+  places = File.read("input.txt").split("\n").map { |line| line.split("") }
+
+  $visible_seats = places.each_with_index.map do |row, row_index|
+    row.each_with_index.map do |place, column_index|
+      visible_seats(places, row_index, column_index)
+    end
+  end
+
   places = next_places(places)
+
+  sum = places.reduce(0) do |sum,row|
+    sum += row.count { |e| e == "#" }
+  end
+  p sum
 }
 
-sum = places.reduce(0) do |sum,row|
-  sum += row.count { |e| e == "#" }
-end
-p sum
 p time.real
