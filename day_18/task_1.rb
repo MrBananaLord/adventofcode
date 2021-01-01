c09bacce02bdeb9e0c343c27e1689d4e14e75078
jan-bator <jan.bator@xing.com>
2021-01-01 18:07:10 +0100

    Day 18.

---
+++ day_18/task_1.rb
@@ -0,0 +1,33 @@
+require 'pry'
+
+sum = File.read("input.txt").split("\n").inject(0) do |sum, line|
+  stack = []
+
+  line.chars.each do |char|
+    next if char == " "
+
+    if %w[* + (].include?(char)
+      stack.push(char)
+    elsif char == ")"
+      until stack[-2] == "(" do
+        stack.push(stack.pop.public_send(stack.pop, stack.pop))
+      end
+
+      stack.delete_at(-2)
+
+      if %w[* +].include?(stack[-2])
+        stack.push(stack.pop.public_send(stack.pop, stack.pop))
+      end
+    else
+      if %w[* +].include?(stack.last)
+        stack.push(char.to_i.public_send(stack.pop, stack.pop))
+      else
+        stack.push(char.to_i)
+      end
+    end
+  end
+
+  sum += stack.first
+end
+
+p sum
